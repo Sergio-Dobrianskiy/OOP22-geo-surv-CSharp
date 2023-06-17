fa20e24667aa37ea515b3bf337c69387f8666300
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-06-17 16:34:38 +0200

    Added IObservable

---
+++ Motta/IObservable.cs
@@ -0,0 +1,29 @@
+namespace Motta
+{
+
+    /// <summary>
+    /// Interface for Observable.
+    /// </summary>
+    public interface IObservable
+    {
+        /// <summary>
+        /// Adds an observer to the player.
+        /// </summary>
+        /// <param name="observer"></param>
+
+        void AddObserver(IObserverEntity<GameObject> observer);
+
+        /// <summary>
+        /// Removes an observer from the player.
+        /// </summary>
+        /// <param name="observer"></param>
+
+        void RemoveObserver(IObserverEntity<GameObject> observer);
+
+        /// <summary>
+        /// Notifies each observer.
+        /// </summary>
+
+        void NotifyObservers();
+    }
+}
