4e9d3cd2ab355a80d4a5f659fe64df7d8428e1f4
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-06-17 16:30:24 +0200

    Added IObserverEntity

---
+++ Motta/IObserverEntity.cs
@@ -0,0 +1,14 @@
+namespace Motta
+{
+    /// <summary>
+    /// Interface for observers.
+    /// </summary>
+    /// <param name="T">specific type for the observer.</param>
+    public interface IObserverEntity<T>
+    {
+       /// <summary>
+       /// Allow to update observers/monsters to follow the player.
+       /// </summary>
+        void Update();
+    }
+}
