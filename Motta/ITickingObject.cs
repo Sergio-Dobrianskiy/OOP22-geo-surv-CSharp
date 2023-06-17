67106bfc22f4aa73782ba9b51cc8390aac866c65
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-06-17 16:28:05 +0200

    Added ITickingObject

---
+++ Motta/ITickingObject.cs
@@ -0,0 +1,13 @@
+namespace Motta;
+
+/// <summary>
+/// Interface for Objects that need a tick method.
+///</summary>>
+ 
+public interface ITickingObject
+{
+    /// <summary>
+    /// Executes object's behavior.
+    /// </summary>
+    void Tick();
+}
