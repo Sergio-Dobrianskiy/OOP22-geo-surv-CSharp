d59dc4a58a30d14f931782e471b73791502527ab
Pierangelo-Motta <pierangelo.motta@studio.unibo.it>
2023-06-18 17:31:57 +0200

    Added Texture (without not needed functions) just to correct GameObject (and IGameObject)

---
+++ Motta/Texture.cs
@@ -0,0 +1,30 @@
+namespace Motta;
+
+///<summary>
+///Enum that contains all maps.
+///</summary>
+public enum Texture
+{
+    Block,
+    BigMap,
+    BigMap2,
+    Bullet,
+    Experience,
+    Explosion,
+    LaserH,
+    LaserV,
+    Life,
+    PlayerDuck,
+    PlayerMouse,
+    Rhombus,
+    Satellite,
+    SmallMap,
+    MissingTexture,
+    Square,
+    SquareBig,
+    TestMap,
+    Triangle,
+    TriangleBig,
+    Ball,
+    BallBig
+}
