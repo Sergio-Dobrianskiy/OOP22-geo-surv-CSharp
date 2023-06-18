4adb7ac199d4ae3801e4e404bff4156ef9fb4206
Sergio Dobrianskiy <sergio.dobrianskiy@studio.unibo.it>
2023-06-18 14:03:25 +0200

    added Bullets and Collissions

--- Dobrianskiy/GameObject.cs
+++ Dobrianskiy/GameObject.cs
@@ -1,4 +1,6 @@
-﻿namespace Dobrianskiy;
+﻿using System.Drawing;
+
+namespace Dobrianskiy;
 
 using Motta;
 
@@ -9,6 +11,8 @@ public class GameObject : ITickingObject
     protected ID id;
     protected int height;
     protected int width;
+    protected float velX;
+    protected float velY;
     public GameObject(float x, float y, ID id)
     {
         this.x = x;
@@ -19,5 +23,45 @@ public class GameObject : ITickingObject
     public void Tick()
     {
         throw new NotImplementedException();
+    }
+    
+    public float GetX()
+    {
+        return x;
+    }
+
+    public void SetX(float x)
+    {
+        this.x = x;
+    }
+
+    public float GetY()
+    {
+        return y;
+    }
+
+    public void SetY(float y)
+    {
+        this.y = y;
+    }
+    
+    public ID GetId()
+    {
+        return id;
+    }
+    
+    public int GetRenderX()
+    {
+        return (int)x - width / 2;
+    }
+
+    public int GetRenderY()
+    {
+        return (int)y - height / 2;
+    }
+    
+    public Rectangle GetShape()
+    {
+        return new Rectangle(GetRenderX(), GetRenderY(), width, height);
     }
 }
